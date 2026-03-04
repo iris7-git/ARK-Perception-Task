76613d91718fc277d6cce4424097f979a23c15d4
Iris7 <harshil.mishra2007@gmail.com>
2026-03-04 22:54:37 +0530

    use adaptive filter instead of bilateral

--- NoiseFilter2.py
+++ NoiseFilter2.py
@@ -1,20 +1,34 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-# Load image (color)
+
+# Load image
 img = cv2.imread("noisy.jpg")
 
-# Step 1: Median filter
-median = cv2.medianBlur(img, 5)
 
-# Step 2: Bilateral filter
-cleaned = cv2.bilateralFilter(median, d=2, sigmaColor=100, sigmaSpace=75)
+img_float = img.astype(np.float32)
+mean = cv2.blur(img_float, (3,3))
+diff = np.abs(img_float - mean)
+threshold = 70 #can have a seperate thresshold for each colour but doesn't help.
+               #since all colours are almost equal in noise
+mask = diff > threshold
+img_float[mask] = mean[mask]
+adaptive = np.uint8(img_float)
 
 
 cv2.imshow("Original",img)
-cv2.imshow("scenary_cleaned", cleaned)
+cv2.imshow("Adaptive",adaptive)
+
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-cv2.imwrite("noisy_cleaned.jpg",cleaned)
+
+
+cv2.imwrite("noisy_cleaned.jpg",adaptive)
+
+
 print("Processing complete. Cleaned image saved as 'noisy_cleaned.jpg'")
+
+
 
