19922632d3c6f252638981b28bb1e9f73c1febf6
Iris7 <harshil.mishra2007@gmail.com>
2026-03-04 16:27:40 +0530

    improved spacing and indentation of code

--- NoiseFilter.py
+++ NoiseFilter.py
@@ -12,33 +12,28 @@ if img is None:
     exit()
 
 
-#print(np.unique(img)) was used to check threshold
+# print(np.unique(img)) was used to check threshold
 _, binary = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY) #127 is the mid value
 
-
-# STEP 3: Connected Component Analysis
 
 num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(
     binary,
     connectivity=8
 )
 
-# Create blank output image
 cleaned = np.zeros_like(binary)
 
-# Skip label 0 (background)
+# label 0 (background)
 for i in range(1, num_labels):
     area = stats[i, cv2.CC_STAT_AREA]
 
-    # Keep only sufficiently large components
     if area >= MIN_COMPONENT_AREA:
         cleaned[labels == i] = 255
 
 
-# STEP 4: Save Output
 cv2.imwrite("iron_man_cleaned.jpg", cleaned)
 
-# STEP 5: Display Comparison
+
 cv2.imshow("Original", img)
 cv2.imshow("Cleaned", cleaned)
 
