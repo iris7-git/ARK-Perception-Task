8527981bc23cadd38ba42507010354f755d7b648
Iris7 <harshil.mishra2007@gmail.com>
2026-03-05 22:57:27 +0530

    add backgroundsubtractormog2

--- Medial Axis/MedialAxis.py
+++ Medial Axis/MedialAxis.py
@@ -1,11 +1,19 @@
 import cv2
-vid = cv2.VideoCapture("1.mp4")
+vid = cv2.VideoCapture("Medial Axis/1.mp4")
 
-count, success = 0, True
-while success:
-    success, image = vid.read() # Read frame
-    if success: 
-        cv2.imwrite(f"frame{count}.jpg", image) # Save frame
-        count += 1
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+while(1):
+    ret, frame = vid.read()
+    if not ret: 
+        print('Video Ended.')
+        break
+    fgmask = fgbg.apply(frame)
+    cv2.imshow('frame',fgmask)
+    k = cv2.waitKey(30) & 0xff #for Esc
+    if k == 27:
+        break
+
 
 vid.release()
+cv2.destroyAllWindows()
