4902691b76b1c83c8ff67d9217897209322f32f8
Iris7 <harshil.mishra2007@gmail.com>
2026-03-06 14:51:16 +0530

    view all videos and choose kernel size to be (6,6)

--- Medial Axis/MedialAxis.py
+++ Medial Axis/MedialAxis.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-vid = cv2.VideoCapture("Medial Axis/1.mp4")
+vid = cv2.VideoCapture("Medial Axis/3.mp4")
 
 fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
 
@@ -11,11 +11,8 @@ while(1):
         print('Video Ended.')
         break
     fgmask = fgbg.apply(frame)
-    # remove weak gray noise
-    #_, fgmask = cv2.threshold(fgmask, 200, 255, cv2.THRESH_BINARY)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(7,7))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(6,6))
     opening = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-    #closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
 
     cv2.imshow('frame',opening)
     k = cv2.waitKey(30) & 0xff #for Esc
