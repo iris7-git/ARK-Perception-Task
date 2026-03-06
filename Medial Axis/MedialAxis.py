37a987053042a01071d5b76ffbf5824d5911aa7d
Iris7 <harshil.mishra2007@gmail.com>
2026-03-06 14:58:41 +0530

    add edge detection using sobel

--- Medial Axis/MedialAxis.py
+++ Medial Axis/MedialAxis.py
@@ -14,7 +14,20 @@ while(1):
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(6,6))
     opening = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 
-    cv2.imshow('frame',opening)
+    gray = opening
+
+    blur = cv2.GaussianBlur(gray,(3,3),0)
+
+    sobelx = cv2.Sobel(blur, cv2.CV_64F, 1, 0, ksize=3)
+    sobely = cv2.Sobel(blur, cv2.CV_64F, 0, 1, ksize=3)
+
+    abs_x = cv2.convertScaleAbs(sobelx)
+    abs_y = cv2.convertScaleAbs(sobely)
+
+    edges = cv2.addWeighted(abs_x,0.5,abs_y,0.5,0)
+
+    #cv2.imshow('frame',opening)
+    cv2.imshow("Edges", edges)
     k = cv2.waitKey(30) & 0xff #for Esc
     if k == 27:
         break
